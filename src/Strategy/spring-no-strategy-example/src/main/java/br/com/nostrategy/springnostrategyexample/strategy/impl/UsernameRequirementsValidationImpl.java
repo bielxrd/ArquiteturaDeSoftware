5367d86065fddb7b03ed4e5e585ce8f5def27249
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-12 15:03:20 -0300

    valida se o username possui regex

---
+++ src/Strategy/spring-no-strategy-example/src/main/java/br/com/nostrategy/springnostrategyexample/strategy/impl/UsernameRequirementsValidationImpl.java
@@ -0,0 +1,19 @@
+package br.com.nostrategy.springnostrategyexample.strategy.impl;
+
+import br.com.nostrategy.springnostrategyexample.dto.NewUserRequest;
+import br.com.nostrategy.springnostrategyexample.model.exceptions.PasswordRegexException;
+import br.com.nostrategy.springnostrategyexample.strategy.NewAccountValidationStrategy;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsernameRequirementsValidationImpl implements NewAccountValidationStrategy {
+
+    @SneakyThrows
+    @Override
+    public void execute(NewUserRequest newUserRequest) {
+        if(!newUserRequest.getUsername().matches("^[a-zA-Z]+$")){
+            throw new PasswordRegexException("O nome deve conter apenas letras.");
+        }
+    }
+}
