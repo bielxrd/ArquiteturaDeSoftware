a7562db66d709cb5d587e8424316d0b9d80ec00e
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-12 15:02:39 -0300

    valida se possui os caracteres especiais e maiusclos

---
+++ src/Strategy/spring-no-strategy-example/src/main/java/br/com/nostrategy/springnostrategyexample/strategy/impl/PasswordRequirementsValidationImpl.java
@@ -0,0 +1,19 @@
+package br.com.nostrategy.springnostrategyexample.strategy.impl;
+
+import br.com.nostrategy.springnostrategyexample.dto.NewUserRequest;
+import br.com.nostrategy.springnostrategyexample.model.exceptions.PasswordRegexException;
+import br.com.nostrategy.springnostrategyexample.repository.UserRepository;
+import br.com.nostrategy.springnostrategyexample.strategy.NewAccountValidationStrategy;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordRequirementsValidationImpl implements NewAccountValidationStrategy {
+    @SneakyThrows
+    @Override
+    public void execute(NewUserRequest newUserRequest) {
+        if(!newUserRequest.getPassword().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).+$")){
+            throw new PasswordRegexException("A senha deve ter pelo menos: 1 caractere minusculo, 1 caractere maiusculo, 1 numero e 1 caractere especial");
+        }
+    }
+}
