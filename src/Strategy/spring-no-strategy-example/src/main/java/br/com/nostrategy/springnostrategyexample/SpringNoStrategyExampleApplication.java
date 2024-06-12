512988830e6a8aa57f29c8c40af1b9b275977772
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-12 02:43:17 -0300

    arquivos gerados pelo initializre

---
+++ src/Strategy/spring-no-strategy-example/src/main/java/br/com/nostrategy/springnostrategyexample/SpringNoStrategyExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.nostrategy.springnostrategyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNoStrategyExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNoStrategyExampleApplication.class, args);
+	}
+
+}
---
+++ src/Strategy/spring-no-strategy-example/src/test/java/br/com/nostrategy/springnostrategyexample/SpringNoStrategyExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nostrategy.springnostrategyexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNoStrategyExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
